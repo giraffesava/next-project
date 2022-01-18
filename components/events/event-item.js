0bd50897b39697993af88581a6205c57ded2195a
Sava <savely54@mail.ru>
2022-01-19 00:27:52 +0700

    added event-item component

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,8 @@
-import Link from "next/link";
+import Button from "../ui/button";
+import classes from "./event-item.module.css";
+import DateIcon from "./../icons/date-icon";
+import AddressIcon from "./../icons/address-icon";
+import ArrowRightIcon from "./../icons/arrow-right-icon";
 
 export default function EventItem(props) {
   console.log(props, "props");
@@ -11,22 +15,27 @@ export default function EventItem(props) {
   const formattedAddress = location.replace(", ", "\n");
   const exploreLink = `/events/${id}`;
   return (
-    <li>
+    <li className={classes.item}>
       <img src={"/" + image} alt="" />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
+            <DateIcon />
             <time>{readableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
-          <Link href={`${exploreLink}`}>
-            <a>Explore Link</a>
-          </Link>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
