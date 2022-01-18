f610c0a9c9e7b799e475148dfdc6ae9570f389e4
Sava <savely54@mail.ru>
2022-01-19 00:28:10 +0700

    adding styles

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,9 +1,10 @@
 import EventItem from "./event-item";
+import classes from "./event-list.module.css";
 
 export default function EventList(props) {
   const { items } = props;
   return (
-    <ul>
+    <ul className={classes.item}>
       {items.map((event) => (
         <EventItem
           key={event.id}
