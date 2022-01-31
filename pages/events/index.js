9047ceb3c1c7580104e91fa046d9ae857bb6c07a
Sava <savely54@mail.ru>
2022-02-01 01:28:54 +0700

    added getStaticProps

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,11 @@
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-utils";
 import EventList from "./../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { useRouter } from "next/router";
-export default function AllEventsPage() {
+
+export default function AllEventsPage({ events }) {
   const router = useRouter();
-  const events = getAllEvents();
+
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
     router.push(fullPath);
@@ -15,4 +16,14 @@ export default function AllEventsPage() {
       <EventList items={events} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return {
+    props: {
+      events,
+    },
+    revalidate: 1800,
+  };
 }
