8f480d178f2ef58a03ab0987a9050b45224e3a02
Sava <savely54@mail.ru>
2022-02-01 01:28:37 +0700

    added revalidate, blocking paging, loading

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,11 +1,15 @@
-import { getEventById, getAllEvents } from "./../../helpers/api-utils";
+import { getEventById, getFeaturedEvents } from "./../../helpers/api-utils";
 import EventSummary from "./../../components/event-detail/event-summary";
 import EventLogistics from "./../../components/event-detail/event-logistics";
 import EventContent from "./../../components/event-detail/event-content";
 
 export default function EventDetailPage({ selectedEvent }) {
   if (!selectedEvent) {
-    return <p>No event found!</p>;
+    return (
+      <div className="center">
+        <p>Loading...</p>
+      </div>
+    );
   }
   return (
     <>
@@ -31,15 +35,16 @@ export async function getStaticProps(context) {
     props: {
       selectedEvent: event,
     },
+    revalidate: 30,
   };
 }
 
 // We need to add it for next, next needs to understand which pages have to pre-render
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
   const paths = events.map((event) => ({ params: { eventId: event.id } }));
   return {
     paths,
-    fallback: false, // Are there any different pages except we have in the array?
+    fallback: "blocking", // Are there any different pages except we have in the array? true - to render data, false - 404 page, 'blocking' - wait untill page will be ready and then show us content
   };
 }
