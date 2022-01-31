6a7e01057d0945b260114671e44559acccda46c7
Sava <savely54@mail.ru>
2022-01-31 23:56:41 +0700

    added static props

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,29 +1,45 @@
-import { useRouter } from "next/router";
-import { getEventById } from "./../../dummy-data";
+import { getEventById, getAllEvents } from "./../../helpers/api-utils";
 import EventSummary from "./../../components/event-detail/event-summary";
 import EventLogistics from "./../../components/event-detail/event-logistics";
 import EventContent from "./../../components/event-detail/event-content";
-export default function EventDetailPage() {
-  const router = useRouter();
 
-  const { eventId } = router.query;
-  const event = getEventById(eventId);
-
-  if (!event) {
+export default function EventDetailPage({ selectedEvent }) {
+  if (!selectedEvent) {
     return <p>No event found!</p>;
   }
   return (
     <>
-      <EventSummary title={event.title} />
+      <EventSummary title={selectedEvent.title} />
       <EventLogistics
-        date={event.date}
-        address={event.location}
-        image={event.image}
-        imageAlt={event.title}
+        date={selectedEvent.date}
+        address={selectedEvent.location}
+        image={selectedEvent.image}
+        imageAlt={selectedEvent.title}
       />
       <EventContent>
-        <p>{event.description}</p>
+        <p>{selectedEvent.description}</p>
       </EventContent>
     </>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+}
+
+// We need to add it for next, next needs to understand which pages have to pre-render
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+  return {
+    paths,
+    fallback: false, // Are there any different pages except we have in the array?
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import EventList from "../components/events/event-list";
-import { getFeaturedEvents } from "./../dummy-data";
-
-export default function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+import { getFeaturedEvents } from "./../helpers/api-utils";
+export default function HomePage({ events }) {
   return (
     <>
-      <EventList items={featuredEvents} />
+      <EventList items={events} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
