a8b92ee22eb4d138f3971f331cbf377529ce2d1e
Sava <savely54@mail.ru>
2022-02-01 15:27:29 +0700

    Image optimization, changed from img to Image

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -3,6 +3,7 @@ import classes from "./event-item.module.css";
 import DateIcon from "./../icons/date-icon";
 import AddressIcon from "./../icons/address-icon";
 import ArrowRightIcon from "./../icons/arrow-right-icon";
+import Image from "next/image";
 
 export default function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -15,7 +16,7 @@ export default function EventItem(props) {
   const exploreLink = `/events/${id}`;
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt="" />
+      <Image src={"/" + image} alt={title} width={340} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
