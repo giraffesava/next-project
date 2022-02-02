f5e046b4e45afb6ac526196856e75e9ceff1bc10
Sava <savely54@mail.ru>
2022-02-02 16:39:41 +0700

    added db helper

---
+++ helpers/db-util.js
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+
+export async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://sava:12344321@cluster0.uzxo5.mongodb.net/events?retryWrites=true&w=majority"
+  );
+  return client;
+}
+export async function insertDocument(client, collection, document) {
+  const db = client.db();
+  const result = await db.collection(collection).insertOne(document);
+  return result;
+}
+
+export async function getAllDocuments(client, collection, sort) {
+  const db = client.db();
+  const documents = await db.collection(collection).find().sort(sort).toArray();
+  return documents;
+}
